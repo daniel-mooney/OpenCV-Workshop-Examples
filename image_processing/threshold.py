f77918ca6429d17a2bf83f8c24f3a553632f656f
daniel-mooney <daniel.mooney180@gmail.com>
2022-07-20 14:52:05 +1000

    applied matplotlib to thresh

--- image_processing/threshold.py
+++ image_processing/threshold.py
@@ -1,18 +1,28 @@
 import cv2 as cv
+import matplotlib.pyplot as plt
 
 def image() -> None:
     img = cv.imread("Images\Outdoor-Toy-Storage.jpg")
     hsv_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)                        # Convert image to HSV image
+    rgb_img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 
     # Bounds for the colour orange in HSV format
     orange_min = (10, 100, 200)
     orange_max = (20, 255, 255)
 
     orange_thresh = cv.inRange(hsv_img, orange_min, orange_max)         # Create Threshold
-    cv.imshow("Orange Threshold", orange_thresh)
-    cv.imshow("Original", img)
+    
+    plt.subplot(121)
+    plt.imshow(rgb_img), plt.title("Original")
+    plt.xticks([]), plt.yticks([])
 
-    cv.waitKey(0)
+    plt.subplot(122)
+    plt.imshow(orange_thresh), plt.title("Orange Threshold")
+    plt.xticks([]), plt.yticks([])
+
+    plt.suptitle("Colour Thresholding")
+    plt.tight_layout()
+    plt.show()
 
 def video() -> None:
     capture = cv.VideoCapture(1)        # front cam
