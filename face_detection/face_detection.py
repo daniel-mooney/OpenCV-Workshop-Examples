0717bd3266836d32a4b40d6f0e3e69a1f2419f4a
daniel-mooney <daniel.mooney180@gmail.com>
2022-07-21 14:53:57 +1000

    renamed to face detection, partially done.

---
+++ face_detection/face_detection.py
@@ -0,0 +1,51 @@
+import cv2 as cv
+
+def detect_objects(frame: cv.Mat, cascade) -> cv.Mat:
+    """
+    Returns a frame with bounding boxes around any detected faces.
+    Detects any objects specified by the type of cascade passed as an argument.
+    """
+    frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    frame_gray = cv.equalizeHist(frame_gray)            # Increase image contrast
+    frame_copy = frame.copy()
+
+    # Detect objects
+    objects = cascade.detectMultiScale(frame_gray)
+
+    for (x,y,h,w) in objects:
+        # x,y are the coordinates of the top left corner
+        frame_copy = cv.rectangle(frame_copy, (x,y), (x+w, y+h), (0,255,0), 2)
+    
+    return frame_copy
+
+def main() -> None:
+    camera = cv.VideoCapture(1)     # front camera
+
+    # Initialise cascade classifier object and load classifier file
+    face_cascade = cv.CascadeClassifier()
+    success = face_cascade.load("haarcascade_frontalface_alt.xml")
+
+    if not success:
+        print("Failed to open face cascade file")
+        return None
+
+    while True:
+        retval, frame = camera.read()
+        frame = cv.flip(frame, 1)
+
+        if not retval:
+            break
+
+        face_frame = detect_objects(frame, face_cascade)
+        
+        cv.imshow("Face Detector", face_frame)
+
+        if cv.waitKey(1) == ord('d'):
+            break
+    
+    camera.release()
+    cv.destroyAllWindows()
+    return None
+
+if __name__ == "__main__":
+    main()
