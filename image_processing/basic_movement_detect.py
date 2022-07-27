f3757bb4077f979619d259bb49ae3bcd8f644fa1
daniel-mooney <daniel.mooney180@gmail.com>
2022-07-27 14:15:16 +1000

    added contour detection

--- image_processing/basic_movement_detect.py
+++ image_processing/basic_movement_detect.py
@@ -1,16 +1,41 @@
 import cv2 as cv
 
 def rescale(image: cv.Mat, scale: float) -> cv.Mat:
-    height = int(image.shape[:2] * scale)
-    width = int(image.shape[:2] * scale)
+    height = int(image.shape[0] * scale)
+    width = int(image.shape[1] * scale)
 
     return cv.resize(image, (width, height), interpolation=cv.INTER_AREA)
+
+def filter_contours(contours, min_area) -> list:
+    filtered_contours = []
+
+    for cnt in contours:
+        if cv.contourArea(cnt) > min_area:
+            filtered_contours.append(cnt)
+
+    return filtered_contours
+
+def get_contours(image, min_area) -> list:
+    contours, hierachy = cv.findContours(image, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    contours = filter_contours(contours, min_area)
+
+    return contours
+
+def draw_bounding_rect(image, contours) -> cv.Mat:
+    image_copy = image.copy()
+    colour = (0,255,0)
+
+    for cnt in contours:
+        x, y, w, h = cv.boundingRect(cnt)
+        cv.rectangle(image_copy, (x,y), (x+w, y+h), colour, 2)
+    
+    return image_copy
 
 def main() -> None:
     file_path = "Videos\people_walking.mp4"
     capture = cv.VideoCapture(file_path)
 
-    background_sub = cv.createBackgroundSubtractorKNN(detectShadows=False)
+    background_sub = cv.createBackgroundSubtractorMOG2()
 
     while True:
         retval, frame = capture.read()
@@ -19,8 +44,12 @@ def main() -> None:
             break
 
         bg_mask = background_sub.apply(frame)
+        retval, bg_mask = cv.threshold(bg_mask, 200, 255, cv.THRESH_BINARY)
 
-        cv.imshow("People Walking", bg_mask)
+        contours = get_contours(bg_mask, 1000)
+
+        cv.imshow("Background Mask", rescale(bg_mask, 0.7))
+        cv.imshow("People Walking", rescale(frame, 0.7))
 
         key = cv.waitKey(17)
         if key == ord('p'):
