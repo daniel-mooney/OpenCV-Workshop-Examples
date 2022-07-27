7356a489abf09eeefcab4420c838e1c84791089d
daniel-mooney <daniel.mooney180@gmail.com>
2022-07-27 13:49:48 +1000

    created main structure

--- image_processing/basic_movement_detect.py
+++ image_processing/basic_movement_detect.py
@@ -4,5 +4,24 @@ def main() -> None:
     file_path = "Videos\people_walking.mp4"
     capture = cv.VideoCapture()
 
+    while True:
+        retval, frame = capture.read()
+
+        if not retval:
+            break
+
+        cv.imshow("People Walking", frame)
+
+        key = cv.waitKey(17)
+        if key == ord('p'):
+            key = cv.waitKey(0)
+        if key == ord('d'):
+            break
+    
+    capture.release()
+    cv.destroyAllWindows()
+
+    return None
+
 if __name__ == "__main__":
     main()
